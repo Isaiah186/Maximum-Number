cf1e54802fc3718e672841d5b80931f453102000
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-11-06 18:30:31 -0500

    Add files via upload

---
+++ Max of 2 Numbers Class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+class Max:
+    def __init__(self):
+        pass
+    
+    def maximum(self,num1,num2):
+        if num1>num2:
+            return num1
+        else:
+            return num2
+
+
+# In[10]:
+
+
+p1 = Max()
+
+
+# In[11]:
+
+
+p1.maximum(20,41)
+
+
+# In[ ]:
+
+
+
+
